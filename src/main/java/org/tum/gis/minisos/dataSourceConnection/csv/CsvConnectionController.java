fa7581d3fb771fd449c271c3f0343b718208cd8a
Kanishk <kanishk.chaturvedi@tum.de>
2018-01-11 15:15:47 +0100

    Fixed CsvConnection POST requests

--- src/main/java/org/tum/gis/minisos/dataSourceConnection/csv/CsvConnectionController.java
+++ src/main/java/org/tum/gis/minisos/dataSourceConnection/csv/CsvConnectionController.java
@@ -1,5 +1,6 @@
 package org.tum.gis.minisos.dataSourceConnection.csv;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CsvConnectionController {
 
+	@Autowired
 	private CsvConnectionService csvConnectionService;
 	
 	@RequestMapping(method=RequestMethod.POST, value="/datasources")
--- src/main/java/org/tum/gis/minisos/dataSourceConnection/csv/CsvConnectionService.java
+++ src/main/java/org/tum/gis/minisos/dataSourceConnection/csv/CsvConnectionService.java
@@ -3,6 +3,7 @@ package org.tum.gis.minisos.dataSourceConnection.csv;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.tum.gis.minisos.dataSource.DataSource;
 import org.tum.gis.minisos.dataSource.DataSourceService;
@@ -10,13 +11,14 @@ import org.tum.gis.minisos.dataSource.DataSourceService;
 @Service
 public class CsvConnectionService {
 
+	@Autowired
 	private DataSourceService dataSourceService;
 	
-	public void addDataSource(CsvConnection csvConnection) {
-		System.out.println("Step2:"+csvConnection.getId());
-		DataSource data3 = new DataSource(3,csvConnection);
-		System.out.println("Step3:"+data3.getId());
-		dataSourceService.addDataSource(data3);
+	public void addDataSource(CsvConnection csvConnection) {	
+		
+		DataSource dataSource = new DataSource(3,csvConnection);
+		
+		dataSourceService.addDataSource(dataSource);
 		
 	}
 
