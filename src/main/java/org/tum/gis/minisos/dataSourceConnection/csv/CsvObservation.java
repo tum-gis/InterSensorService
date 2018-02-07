e581717cd4ce14968b0a1270da23967407f33bf4
Kanishk <kanishk.chaturvedi@tum.de>
2018-02-07 17:36:16 +0100

    Modified CsvObservation class

---
+++ src/main/java/org/tum/gis/minisos/dataSourceConnection/csv/CsvObservation.java
@@ -0,0 +1,23 @@
+package org.tum.gis.minisos.dataSourceConnection.csv;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.tum.gis.minisos.observation.Observation;
+import org.tum.gis.minisos.util.CustomDateUtil;
+import org.tum.gis.minisos.util.ObservationValueUtil;
+
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
+
+public class CsvObservation extends Observation  {
+	
+	
+	private String time;
+	
+	private double value;
+	
+
+}
