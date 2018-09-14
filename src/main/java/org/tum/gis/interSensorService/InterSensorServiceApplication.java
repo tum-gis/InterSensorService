0dcd8a5b28656211e4663b987bc747a17f498003
kanishk-chaturvedi <kanishk.chaturvedi@tum.de>
2018-09-14 10:06:36 +0200

    Updated License Details

--- src/main/java/org/tum/gis/interSensorService/InterSensorServiceApplication.java
+++ src/main/java/org/tum/gis/interSensorService/InterSensorServiceApplication.java
@@ -1,3 +1,29 @@
+/*
+ * InterSensor Service
+ * http://www.intersensorservice.org/
+ * 
+ * Copyright 2018
+ * Chair of Geoinformatics
+ * Technical University of Munich, Germany
+ * https://www.gis.bgu.tum.de/
+ * 
+ * The InterSensor Service has been developed by
+ * Kanishk Chaturvedi and Thomas H. Kolbe
+ * 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
 package org.tum.gis.interSensorService;
 
 import java.io.IOException;
